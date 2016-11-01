e93a8fff3b42f0fc3f9d39ebd26238db65ca3f2b
TheFunnyFunyan <brando15@ksu.edu>
2016-11-01 14:13:38 -0500

    finished and ready to go

--- modifier.py
+++ modifier.py
@@ -1,19 +1,46 @@
 import os
 import re
 
+rootdir = 'C:/Users/Brandon/DirectoryOrganizer/testDir'
+
 def main():
-  rootdir = 'd:/Users/brandon.runyan/fileModifier'
   for root, dirs, files in os.walk(rootdir):
-    print(root)
     if(len(dirs)>0):
       continue 
+
+    rootName = getParentName(root)
     if(len(files)>1):
-      for file in files:
-        index = 1
-        print(getParentName(root) + str(index))
+      index = 1
+      for singleFile in files:
+        fileExt = getFileExt(singleFile)
+        newPath = getFilePath(rootName, fileExt, str(index))
+        oldPath = getOldFilePath(root, singleFile, fileExt)
+        renameFiles(oldPath, newPath)
         index += 1
+    else:
+      fileExt = getFileExt(files[0])
+      newPath = getFilePath(rootName, fileExt)
+      oldPath = getOldFilePath(root, files[0], fileExt)
+      renameFiles(oldPath, newPath)
+    removeOldDir(root)
 
   print("done")
+
+def removeOldDir(rootPath:str) -> str:
+  os.rmdir(rootPath)
+
+def getOldFilePath(rootPath:str, fileName:str, ext:str) -> str:
+  return rootPath + '/' + fileName 
+
+def renameFiles(oldPath:str, newPath:str) -> None:
+  print(oldPath + ' -> ' + newPath)
+  os.rename(oldPath, newPath)
+
+def getFilePath(rootName:str, fileExt:str, index:str='') -> str:
+  return rootdir + '/' + rootName + index + '.' + fileExt
+
+def getFileExt(fileName:str) -> str:
+  return fileName.split('.')[1]
 
 def getParentName(root:str) -> str:
   roots = re.split(r'\\|/', root)
